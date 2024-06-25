f3a2c6720ddc558e37c8364830af39dabb8abb88
José Pablo Pavón Ferrero <josepablo.pavon@sofitec.aero>
2024-06-25 17:12:44 +0200

    Primer commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using ApiPeliculas.Repositorio.IRepositorio;
+using ApiPeliculas.Modelos.Dtos;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers;
+//[Route("api/[controller]")] -- Una opción
+[Route("api/categorias")] // otra opción
+[ApiController]
+public class ValuesController : ControllerBase {
+
+    private readonly ICategoriaRepositorio _ctRepo;
+    private readonly IMapper _mapper;
+
+    public ValuesController(ICategoriaRepositorio ctRepo, IMapper mapper)
+    {
+        _ctRepo = ctRepo;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public IActionResult GetCategorias() {
+        var listaCategorias = _ctRepo.GetCategorias();
+        var listaCategoriasDto = new List<CategoriaDto>();
+
+        foreach(var categoria in listaCategorias) {
+            listaCategoriasDto.Add(_mapper.Map<CategoriaDto>(categoria));
+        }
+
+        return Ok(listaCategoriasDto);
+
+    }
+
+}
---
+++ Data/MiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiPeliculas.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPeliculas.Data;
+
+public class MiDbContext: DbContext{
+
+    public MiDbContext (DbContextOptions<MiDbContext> options) : base(options) { }
+
+    //Agregar los modelos aquí
+
+    public DbSet<Categoria> Categorias { get; set; }
+
+}
